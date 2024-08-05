77660c4d465c8487826a037aa28b602c3230771c
Divyanshu2607 <divyanshuchauhan264@gmail.com>
2024-08-06 01:13:30 +0530

    intial commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:divyanshuchauhan264@email.com">divyanshuchauhan264@email.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/divyanshu-chauhan-461504222/">linkedin.com/divyanshu-chauhan</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/Divyanshu2607">github.com/Divyanshu2607</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
